e46d15c007a0c304b60e7fbd9b4501475d409a44
padillosweedeneshane-byte <padillosweedeneshane@isufst.edu.ph>
2025-10-27 11:54:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def home(): 
- return  "Welcome to my Flask API!"
+def home():
+    return render_template("index.html")
 
 @app.route('/student')
 def get_student():
- return jsonify({
-  "name": "Your Name",
-   "grade": 10,
-   "section": "Zechariah"
-})
+    return jsonify({
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
