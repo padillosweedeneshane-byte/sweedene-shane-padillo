cbeeb362a29961fee11438498d025b1bc7ae3657
padillosweedeneshane-byte <padillosweedeneshane@isufst.edu.ph>
2025-10-27 11:39:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return "Welcome to my Flask API!"
+def home(): return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
